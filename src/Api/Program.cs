26bf6bfc435ed47dcaf7e88a98246303934d9cd4
Hardik Dhuri <hardikdhuri8347@gmail.com>
2024-12-15 21:47:22 +0530

    chore: Add docker files for api, client and worker

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -39,11 +39,8 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-    app.MapScalarApiReference();
-}
+app.MapOpenApi();
+app.MapScalarApiReference();
 
 app.UseHttpsRedirection();
 app.MapControllers();
